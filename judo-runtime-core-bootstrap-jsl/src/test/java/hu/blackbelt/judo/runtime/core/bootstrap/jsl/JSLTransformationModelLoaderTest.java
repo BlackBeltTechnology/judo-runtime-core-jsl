25fd4ed81ce052140b3319e8b28cd37cac273d13
Robert Csakany <robert.csakany@blackbelt.hu>
2022-05-20 18:20:03 +0200

    Add default loader and smoke test

---
+++ judo-runtime-core-bootstrap-jsl/src/test/java/hu/blackbelt/judo/runtime/core/bootstrap/jsl/JSLTransformationModelLoaderTest.java
@@ -0,0 +1,33 @@
+package hu.blackbelt.judo.runtime.core.bootstrap.jsl;
+
+import com.google.common.collect.ImmutableList;
+import hu.blackbelt.judo.tatami.core.workflow.work.TransformationContext;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static hu.blackbelt.judo.tatami.jsl.workflow.DefaultWorkflowSave.saveModels;
+
+class JSLTransformationModelLoaderTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void loadAndTransformJslModels() {
+
+        TransformationContext context = JSLTransformationModelLoader.loadAndTransformJslModels("test2::SalesModel2", "hsqldb",
+                new File("src/test/resources/models/sample.jsl").toURI(),
+                new File("src/test/resources/models/sample2.jsl").toURI()
+                );
+
+        saveModels(context, new File("target/test-classes/loader-test"), ImmutableList.of("hsqldb"));
+    }
+}
