a5c36c7ed1c23bb8d05dac82db1d40f383004249
Robert Csakany <csakany.robert@gmail.com>
2022-06-21 11:33:15 +0200

    Improving tests and reorganizing SpringBoot app

--- judo-runtime-core-jsl-spring-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/spring/JudoRuntimeCoreSpringApplicationTests.java
+++ judo-runtime-core-jsl-spring-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/spring/JudoRuntimeCoreSpringApplicationTests.java
@@ -1,13 +1,77 @@
 package hu.blackbelt.judo.runtime.core.jsl.spring;
 
+import hu.blackbelt.judo.runtime.core.jsl.spring.test.salesmodel.sdk.salesmodel.salesmodel.Lead;
+import hu.blackbelt.judo.runtime.core.jsl.spring.test.salesmodel.sdk.salesmodel.salesmodel.Person;
+import hu.blackbelt.judo.runtime.core.jsl.spring.test.salesmodel.sdk.salesmodel.salesmodel.SalesPerson;
+import hu.blackbelt.judo.runtime.core.jsl.spring.test.salesmodel.sdk.salesmodel.salesmodel._SalesPerson_leadsOver_Parameters;
+import hu.blackbelt.judo.sdk.query.StringFilter;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class JudoRuntimeCoreSpringApplicationTests {
 
+
+	@Autowired
+	Person.PersonDao personDao;
+
+	@Autowired
+	SalesPerson.SalesPersonDao salesPersonDao;
+
+	@Autowired
+	Lead.LeadDao leadDao;
+
 	@Test
-	void contextLoads() {
+	void testDaoFunctions() {
+
+		SalesPerson createdSalesPerson = salesPersonDao.create(SalesPerson.builder()
+				.withFirstName("Test")
+				.withLastName("Elek")
+				.build());
+
+		assertEquals("Test", createdSalesPerson.getFirstName());
+		assertEquals("Elek", createdSalesPerson.getLastName());
+
+		List<SalesPerson> personList = salesPersonDao.search()
+				.filterByFirstName(StringFilter.equalTo("Test"))
+				.execute();
+
+		assertEquals(1, personList.size());
+
+		Person createdPerson = personDao.create(Person.builder()
+				.withFirstName("Masik")
+				.withLastName("Test")
+				.build());
+
+		assertEquals("Masik", createdPerson.getFirstName());
+		assertEquals("Test", createdPerson.getLastName());
+
+		Lead lead1 = leadDao.create(Lead.builder()
+				.withSalesPerson(createdSalesPerson)
+				.build());
+		assertEquals(100000, lead1.getValue());
+		assertEquals("Test", leadDao.getSalesPerson(lead1).getFirstName());
+
+		Lead lead2 = leadDao.create(Lead.builder()
+				.withSalesPerson(createdSalesPerson)
+				.withValue(9)
+				.build());
+		assertEquals(9, lead2.getValue());
+		assertEquals("Test", leadDao.getSalesPerson(lead2).getFirstName());
+
+		List<Lead> leadListOfQuery = salesPersonDao
+				.searchLeadsOver(createdSalesPerson.get__identifier())
+				.execute(_SalesPerson_leadsOver_Parameters.builder()
+						.withLimit(10)
+						.build());
+		assertEquals(1, leadListOfQuery.size());
+		assertEquals(100000, leadListOfQuery.get(0).getValue());
+
 	}
 
 }
