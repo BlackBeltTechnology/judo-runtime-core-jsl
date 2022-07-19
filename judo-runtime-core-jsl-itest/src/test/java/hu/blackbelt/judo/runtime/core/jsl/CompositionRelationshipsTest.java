07f5c84bd5922bb7bf410781b2db6c1b0fca8dbe
Norbert Herczeg <noherczeg@gmail.com>
2022-07-19 18:55:21 +0200

    JNG-3837: add composition tests

--- judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/CompositionRelationshipsTest.java
+++ judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/CompositionRelationshipsTest.java
@@ -55,11 +55,13 @@ public class CompositionRelationshipsTest {
         entityD2 = entityDDao.create(EntityD.builder()
                 .build());
         singleRequiredConA = entityCDao.create(EntityC.builder()
+                .withStringC("TEST-C")
                 .withMultipleDonB(List.of(entityD1, entityD2))
                 .build());
         singleConA = entityCDao.create(EntityC.builder()
                 .build());
         entityA = entityADao.create(EntityA.builder()
+                .withStringA("TEST-A")
                 .withSingleRequiredConA(singleRequiredConA)
                 .withSingleConA(singleConA)
                 .build());
@@ -99,5 +101,58 @@ public class CompositionRelationshipsTest {
 
         assertEquals(Optional.empty(), entityADao.getSingleConA(entityA));
         assertEquals(1, entityCDao.query().execute().size());
+    }
+
+    @Test
+    void testTraverse() {
+        List<EntityA> maskedAs = entityADao.query().execute();
+        EntityA maskedA = maskedAs.get(0);
+        EntityC singleRequiredConA = maskedA.getSingleRequiredConA();
+
+
+        assertEquals(Optional.of("TEST-A"), maskedA.getStringA());
+        assertNotEquals(Optional.empty(), maskedA.getSingleConA());
+        assertNotEquals(null, maskedA.getSingleRequiredConA());
+        assertEquals(Optional.of("TEST-C"), singleRequiredConA.getStringC());
+        assertEquals(2, singleRequiredConA.getMultipleDonB().size());
+        assertEquals(Optional.of(entityD1), singleRequiredConA.getMultipleDonB().stream().filter(d -> d.get__identifier().equals(entityD1.get__identifier())).findFirst());
+        assertEquals(Optional.of(entityD2), singleRequiredConA.getMultipleDonB().stream().filter(d -> d.get__identifier().equals(entityD2.get__identifier())).findFirst());
+    }
+
+    @Test
+    void testMask() {
+        List<EntityA> maskedAs = entityADao.query().maskedBy(EntityA.EntityADao.Mask.entityAMask().withSingleRequiredConA(EntityC.EntityCDao.Mask.entityCMask().withStringC())).execute();
+
+        EntityA maskedA = maskedAs.get(0);
+        EntityC requiredC = maskedA.getSingleRequiredConA();
+
+        assertEquals(1, maskedAs.size());
+        assertEquals(Optional.empty(), maskedA.getSingleConA());
+        assertEquals(Optional.empty(), maskedA.getStringA());
+        assertEquals(singleRequiredConA, maskedA.getSingleRequiredConA());
+        assertEquals(Optional.empty(), requiredC.getStringB());
+        assertEquals(Optional.of("TEST-C"), requiredC.getStringC());
+    }
+
+    @Test
+    void testUpdateRootUpdatesInterimElement() {
+        EntityA entityA2 = entityADao.query().execute().get(0);
+        EntityC requiredC = entityA2.getSingleRequiredConA();
+        EntityC singleC = entityA2.getSingleConA().get();
+
+        assertEquals(Optional.empty(), requiredC.getStringB());
+        assertEquals(Optional.empty(), singleC.getStringB());
+        assertEquals(Optional.empty(), singleC.getStringC());
+        assertEquals(Optional.of(singleConA), entityA2.getSingleConA());
+
+        requiredC.setStringB("Hello!");
+        singleC.setStringB("NEW-B");
+        singleC.setStringC("NEW-C");
+
+        EntityA updatedA = entityADao.update(entityA2);
+
+        assertEquals(Optional.of("Hello!"), updatedA.getSingleRequiredConA().getStringB());
+        assertEquals(Optional.of("NEW-B"), entityA2.getSingleConA().get().getStringB());
+        assertEquals(Optional.of("NEW-C"), entityA2.getSingleConA().get().getStringC());
     }
 }
