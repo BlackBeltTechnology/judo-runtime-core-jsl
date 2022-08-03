6e0244ab470eff271eeaab5f2db1de5d50577d44
Norbert Herczeg <noherczeg@gmail.com>
2022-08-03 15:50:11 +0200

    JNG-3837: add any type function tests

---
+++ judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/FunctionsTest.java
@@ -0,0 +1,96 @@
+package hu.blackbelt.judo.runtime.core.jsl;
+
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import hu.blackbelt.judo.runtime.core.bootstrap.JudoDefaultModule;
+import hu.blackbelt.judo.runtime.core.bootstrap.JudoModelLoader;
+import hu.blackbelt.judo.runtime.core.bootstrap.dao.rdbms.hsqldb.JudoHsqldbModules;
+import hu.blackbelt.judo.runtime.core.dao.rdbms.hsqldb.HsqldbDialect;
+import hu.blackbelt.judo.runtime.core.jsl.itest.functions.guice.functions.FunctionsDaoModules;
+import hu.blackbelt.judo.runtime.core.jsl.itest.functions.sdk.functions.functions.AnyTypeFunctions;
+import hu.blackbelt.judo.runtime.core.jsl.itest.functions.sdk.functions.functions.Entity;
+import hu.blackbelt.judo.runtime.core.jsl.itest.functions.sdk.functions.functions.EntityWithPrimitiveDefaults;
+import hu.blackbelt.judo.runtime.core.jsl.itest.primitives.guice.primitives.PrimitivesDaoModules;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Slf4j
+public class FunctionsTest {
+    Injector injector;
+
+    @Inject
+    Entity.EntityDao entityDao;
+
+    @Inject
+    EntityWithPrimitiveDefaults.EntityWithPrimitiveDefaultsDao entityWithPrimitiveDefaultsDao;
+
+    @Inject
+    AnyTypeFunctions.AnyTypeFunctionsDao anyTypeFunctionsDao;
+
+    AnyTypeFunctions anyTypeFunctions;
+
+    @BeforeEach
+    void init() throws Exception {
+        JudoModelLoader modelHolder = JudoModelLoader.
+                loadFromClassloader("Functions", FunctionsTest.class.getClassLoader(), new HsqldbDialect(), true);
+
+        injector = Guice.createInjector(
+                JudoHsqldbModules.builder().build(),
+                new FunctionsDaoModules(),
+                new JudoDefaultModule(this, modelHolder));
+
+        Entity entity = entityDao
+                        .create(Entity.builder().build());
+        EntityWithPrimitiveDefaults entityWithPrimitiveDefaults = entityWithPrimitiveDefaultsDao
+                        .create(EntityWithPrimitiveDefaults.builder().build());
+
+        anyTypeFunctions = anyTypeFunctionsDao.create(AnyTypeFunctions.builder()
+                        .withEntity(entity)
+                        .withEntityWithPrimitives(entityWithPrimitiveDefaults)
+                        .build());
+    }
+
+    @Test
+    public void testAnyType() {
+        assertEquals(Optional.of(false), anyTypeFunctions.getIntegerIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getIntegerIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getIntegerIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getIntegerIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getScaledIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getScaledIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getScaledIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getScaledIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getStringIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getStringIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getStringIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getStringIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getBoolIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getBoolIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getBoolIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getBoolIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getDateIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getDateIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getDateIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getDateIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getTimestampIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getTimestampIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getTimestampIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getTimestampIsUnDefinedFalse());
+
+        assertEquals(Optional.of(false), anyTypeFunctions.getTimeIsDefinedFalse());
+        assertEquals(Optional.of(true), anyTypeFunctions.getTimeIsDefinedTrue());
+        assertEquals(Optional.of(true), anyTypeFunctions.getTimeIsUnDefinedTrue());
+        assertEquals(Optional.of(false), anyTypeFunctions.getTimeIsUnDefinedFalse());
+    }
+}
