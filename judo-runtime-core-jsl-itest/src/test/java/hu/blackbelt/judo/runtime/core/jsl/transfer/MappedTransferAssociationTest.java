18cd56f6516c63c216b6c8451bdc5c68bb57c8cd
gaborflorian <gabor.florian@blackbelt.hu>
2023-05-25 18:02:20 +0200

    Add testCases for single relation and singleRequired relation

--- judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/transfer/MappedTransferAssociationTest.java
+++ judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/transfer/MappedTransferAssociationTest.java
@@ -22,8 +22,18 @@ package hu.blackbelt.judo.runtime.core.jsl.transfer;
 
 import com.google.inject.Inject;
 import com.google.inject.Module;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.entitya.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.entityb.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.entityc.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.entityd.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.transfera.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.transferb.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.transferc.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.mappedtransferassociation.mappedtransferassociation.transferd.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.guice.MappedTransferAssociationDaoModules;
 import hu.blackbelt.judo.requirement.report.annotation.Requirement;
+import hu.blackbelt.judo.requirement.report.annotation.TestCase;
+import hu.blackbelt.judo.runtime.core.exception.ValidationException;
 import hu.blackbelt.judo.runtime.core.jsl.AbstractJslTest;
 import hu.blackbelt.judo.runtime.core.jsl.fixture.JudoDatasourceFixture;
 import lombok.extern.slf4j.Slf4j;
@@ -33,10 +43,37 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class MappedTransferAssociationTest extends AbstractJslTest {
+
+    @Inject
+    EntityADao entityADao;
+
+    @Inject
+    TransferADao transferADao;
+
+    @Inject
+    EntityBDao entityBDao;
+
+    @Inject
+    TransferBDao transferBDao;
+
+    @Inject
+    EntityCDao entityCDao;
+
+    @Inject
+    TransferCDao transferCDao;
+
+    @Inject
+    EntityDDao entityDDao;
+
+    @Inject
+    TransferDDao transferDDao;
 
 
     @Override
@@ -50,13 +87,94 @@ public class MappedTransferAssociationTest extends AbstractJslTest {
     }
 
     @Test
+    @TestCase("")
     @Requirement(reqs = {
-            "REQ-ENT-001",
-            "REQ-ENT-004",
-            "REQ-ENT-005",
-            "REQ-ENT-012"
+            ""
     })
-    public void testSetUnsetSingleRelation() {
+    public void testSetUnsetSingleRelationOnTransfer() {
+
+        TransferB tb1 = transferBDao.create(TransferB.builder().build());
+        TransferB tb2 = transferBDao.create(TransferB.builder().build());
+
+        TransferA transferA = transferADao.create(TransferA
+                .builder()
+                .withRelationBonA(tb1)
+//                .withRelationConBTwoWay(b2)
+                .build());
+
+        assertEquals(tb1.identifier(),transferA.getRelationBonA().orElseThrow().identifier());
+
+        EntityA a = entityADao.getById(transferA.identifier().adaptTo(EntityAIdentifier.class)).orElseThrow();
+        EntityB b1 = entityBDao.getById(tb1.identifier().adaptTo(EntityBIdentifier.class)).orElseThrow();
+
+        assertEquals(b1.identifier(), entityADao.queryRelationBonA(a).orElseThrow().identifier());
+
+        transferA.setRelationBonA(tb2);
+        transferA = transferADao.update(transferA);
+
+        assertEquals(tb2.identifier(),transferA.getRelationBonA().orElseThrow().identifier());
+
+        EntityB b2 = entityBDao.getById(tb2.identifier().adaptTo(EntityBIdentifier.class)).orElseThrow();
+
+        assertEquals(b2.identifier(), entityADao.queryRelationBonA(a).orElseThrow().identifier());
+
+        transferA.setRelationBonA(null);
+        transferA = transferADao.update(transferA);
+
+        assertTrue(transferA.getRelationBonA().isEmpty());
+        assertTrue(entityADao.queryRelationBonA(a).isEmpty());
+
+    }
+
+    @Test
+    @TestCase("")
+    @Requirement(reqs = {
+            ""
+    })
+    public void testSetUnsetSingleRequeredRelationOnTransfer() {
+
+        TransferD td1 = transferDDao.create(TransferD.builder().build());
+        TransferD td2 = transferDDao.create(TransferD.builder().build());
+
+        TransferC transferC = transferCDao.create(TransferC
+                .builder()
+                .withRelationDonC(td1)
+//                .withRelationConBTwoWay(b2)
+                .build());
+
+        assertEquals(td1.identifier(),transferC.getRelationDonC().identifier());
+
+        EntityC c = entityCDao.getById(transferC.identifier().adaptTo(EntityCIdentifier.class)).orElseThrow();
+        EntityD d1 = entityDDao.getById(td1.identifier().adaptTo(EntityDIdentifier.class)).orElseThrow();
+
+        assertEquals(d1.identifier(), entityCDao.queryRelationDonC(c).identifier());
+
+        transferC.setRelationDonC(td2);
+        transferC = transferCDao.update(transferC);
+
+        assertEquals(td2.identifier(),transferC.getRelationDonC().identifier());
+
+        EntityD d2 = entityDDao.getById(td2.identifier().adaptTo(EntityDIdentifier.class)).orElseThrow();
+
+        assertEquals(d2.identifier(), entityCDao.queryRelationDonC(c).identifier());
+
+        IllegalStateException thrown = assertThrows(
+                IllegalStateException.class,
+                () -> transferDDao.delete(td2)
+        );
+
+        assertTrue(thrown.getMessage().contains("There are mandatory references that cannot be removed"));
+        assertTrue(thrown.getMessage().contains("#relationDonC"));
+
+        ValidationException thrown1 = assertThrows(
+                ValidationException.class,
+                () -> transferCDao.create(TransferC.builder().build())
+        );
+
+        assertThat(thrown1.getValidationResults(), containsInAnyOrder(allOf(
+                hasProperty("code", equalTo("MISSING_REQUIRED_RELATION")),
+                hasProperty("location", equalTo("relationDonC")))
+        ));
 
     }
 
