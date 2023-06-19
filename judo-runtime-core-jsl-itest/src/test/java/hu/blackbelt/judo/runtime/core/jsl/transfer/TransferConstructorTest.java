38f9f981d9430b5b56def2fefa6240b62ce16e0a
gaborflorian <gabor.florian@blackbelt.hu>
2023-06-19 16:06:32 +0200

    Add relation constructor tests

--- judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/transfer/TransferConstructorTest.java
+++ judo-runtime-core-jsl-itest/src/test/java/hu/blackbelt/judo/runtime/core/jsl/transfer/TransferConstructorTest.java
@@ -2,16 +2,20 @@ package hu.blackbelt.judo.runtime.core.jsl.transfer;
 
 import com.google.inject.Inject;
 import com.google.inject.Module;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithmultirelation.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithrequiredprimitivesdefaults.EntityWithRequiredPrimitivesDefaults;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithrequiredprimitivesdefaults.EntityWithRequiredPrimitivesDefaultsDao;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithrequiredprimitivesdefaults.EntityWithRequiredPrimitivesDefaultsIdentifier;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithsinglerelation.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.enum_.Enum;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithprimitives.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithrequiredprimitives.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.entitywithprimitivesdefaults.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.mappedentitywithmultirelation.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.mappedentitywithsinglerelation.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferoptionalprimitiveswithconstructor.*;
-import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferrequiredprimitivesdefaultswithconstructor.TransferRequiredPrimitivesDefaultsWithConstructor;
-import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferrequiredprimitivesdefaultswithconstructor.TransferRequiredPrimitivesDefaultsWithConstructorDao;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferrequiredprimitivesdefaultswithconstructor.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferrequiredprimitivesdefaultswithconstructor.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferrequiredprimitiveswithconstructor.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.transferconstructor.transferconstructor.transferoptionalprimitivesdefaultswithconstructor.*;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.guice.TransferConstructorDaoModules;
@@ -65,6 +69,19 @@ public class TransferConstructorTest extends AbstractJslTest {
 
     @Inject
     TransferRequiredPrimitivesDefaultsWithConstructorDao transferRequiredPrimitivesDefaultsWithConstructorDao;
+
+    @Inject
+    MappedEntityWithMultiRelationDao mappedEntityWithMultiRelationDao;
+
+    @Inject
+    MappedEntityWithSingleRelationDao mappedEntityWithSingleRelationDao;
+
+    @Inject
+    EntityWithSingleRelationDao entityWithSingleRelationDao;
+
+    @Inject
+    EntityWithMultiRelationDao entityWithMultiRelationDao;
+
 
     /**
      * This test check the transfer constructor on an entity with contains primitive optional field.
@@ -377,5 +394,69 @@ public class TransferConstructorTest extends AbstractJslTest {
         assertEquals(Enum.EnumA, entityWithRequiredPrimitivesDefaults.getEnumAttr());
 
     }
+
+    /**
+     * This test check the transfer constructor on an entity with relations.
+     *
+     * @prerequisites The model runtime is empty. It means that the database of the application has to be empty before this test starts running.
+     *
+     * @type Behaviour
+     *
+     * @others Implement this test case in the *judo-runtime-core-jsl-itest* module.
+     *
+     * @jslModel TransferConstructor.jsl
+     *
+     * @positiveRequirements
+     *
+     * @scenario
+     *
+     *  Create one instance of mappedEntityWithMultiRelation.
+     *
+     *  Create one instance of mappedEntityWithSingleRelation.
+     *
+     *  Check the mappedEntityWithSingleRelation instance contains the constructor expressions.
+     *
+     *  Get the entity representation the MappedEntityWithSingleRelation instance.
+     *
+     *  Check the entity representation contains the constructor assigned values.
+     *
+     */
+    @Test
+    public void testTransferAssossiationConstrtuctor() {
+
+        MappedEntityWithMultiRelation mappedEntityWithMultiRelation = mappedEntityWithMultiRelationDao.create(MappedEntityWithMultiRelation.builder().build());
+
+        MappedEntityWithSingleRelation mappedEntityWithSingleRelation = mappedEntityWithSingleRelationDao.create(MappedEntityWithSingleRelation.builder().build());
+
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociation(mappedEntityWithSingleRelation).orElseThrow().identifier(),mappedEntityWithMultiRelation.identifier());
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociationCollection(mappedEntityWithSingleRelation).count(),1);
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociationCollection(mappedEntityWithSingleRelation).execute().get(0).identifier(),mappedEntityWithMultiRelation.identifier());
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociationOpposite(mappedEntityWithSingleRelation).orElseThrow().identifier(),mappedEntityWithMultiRelation.identifier());
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociationOppositeCollection(mappedEntityWithSingleRelation).count(),1);
+        assertEquals(mappedEntityWithSingleRelationDao.queryMappedAssociationOppositeCollection(mappedEntityWithSingleRelation).execute().get(0).identifier(),mappedEntityWithMultiRelation.identifier());
+
+        Optional<EntityWithSingleRelation> entityWithSingleRelationOptional = entityWithSingleRelationDao.getById(mappedEntityWithSingleRelation.adaptTo(EntityWithSingleRelationIdentifier.class));
+
+        assertTrue(entityWithSingleRelationOptional.isPresent());
+
+        EntityWithSingleRelation entityWithSingleRelation = entityWithSingleRelationOptional.orElseThrow();
+
+        EntityWithMultiRelation entityWithMultiRelation = entityWithMultiRelationDao.getById(mappedEntityWithMultiRelation.adaptTo(EntityWithMultiRelationIdentifier.class)).orElseThrow();
+
+        assertEquals(entityWithSingleRelationDao.queryAssociation(entityWithSingleRelation).orElseThrow().identifier(),entityWithMultiRelation.identifier());
+        assertEquals(entityWithSingleRelationDao.queryAssociationCollection(entityWithSingleRelation).count(),1);
+        assertEquals(entityWithSingleRelationDao.queryAssociationCollection(entityWithSingleRelation).execute().get(0).identifier(),entityWithMultiRelation.identifier());
+        assertEquals(entityWithSingleRelationDao.queryEntityWithTwoWayOpposite(entityWithSingleRelation).orElseThrow().identifier(),entityWithMultiRelation.identifier());
+        assertEquals(entityWithSingleRelationDao.queryEntityWithTwoWayOppositeCollection(entityWithSingleRelation).count(),1);
+        assertEquals(entityWithSingleRelationDao.queryEntityWithTwoWayOppositeCollection(entityWithSingleRelation).execute().get(0).identifier(),entityWithMultiRelation.identifier());
+
+
+        //transient relations
+//        assertEquals(mappedEntityWithSingleRelationDao.queryTransientRelation(mappedEntityWithSingleRelation).orElseThrow().identifier(),mappedEntityWithMultiRelation.identifier());
+//        assertEquals(mappedEntityWithSingleRelationDao.queryTransientRelationCollection(mappedEntityWithSingleRelation).count(),1);
+//        assertEquals(mappedEntityWithSingleRelationDao.queryTransientRelationCollection(mappedEntityWithSingleRelation).execute().get(0).identifier(),mappedEntityWithMultiRelation.identifier());
+
+    }
+
 
 }
