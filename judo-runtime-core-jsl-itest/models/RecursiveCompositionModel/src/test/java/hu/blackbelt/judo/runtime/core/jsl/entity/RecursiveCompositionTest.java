2fcbb975b9c0cf38eb2b5c3c961cd3e0e49bce17
Madmat8 <Mateka6400@gmail.com>
2023-08-09 14:07:14 +0200

    add recursive composition test on entity

--- judo-runtime-core-jsl-itest/models/RecursiveCompositionModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/RecursiveCompositionTest.java
+++ judo-runtime-core-jsl-itest/models/RecursiveCompositionModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/RecursiveCompositionTest.java
@@ -31,8 +31,10 @@ import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.guice.RecursiveCompositionDaoModules;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.recursivecomposition.recursivecomposition.entityx.*;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.recursivecomposition.recursivecomposition.entityy.*;
 
-import hu.blackbelt.judo.psm.generator.sdk.core.test.api.compositionrelationships.compositionrelationships.entityf.EntityF;
 
 import java.util.List;
 import java.util.Optional;
@@ -44,6 +46,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 public class RecursiveCompositionTest extends AbstractJslTest {
+
+    @Inject
+    EntityXDao entityXDao;
+
+    @Inject
+    EntityYDao entityYDao;
 
     @Override
     public Module getModelDaoModule() {
@@ -60,15 +68,112 @@ public class RecursiveCompositionTest extends AbstractJslTest {
             "REQ-ENT-001",
             "REQ-ENT-007"
     })
-    void testMissingRequiredRelationshipThrowsException() {
-        ValidationException thrown = assertThrows(
-                ValidationException.class,
-                () -> entityADao.create(EntityA.builder().build())
-        );
+    void testRecursiveCompositionOnEntity() {
+        EntityX x13 = entityXDao.create(EntityX.builder().withName("x13").build());
+        EntityX x11 = entityXDao.create(EntityX.builder().withName("x11").build());
+        EntityX x8 = entityXDao.create(EntityX.builder().withName("x8").build());
+        EntityX x5 = entityXDao.create(EntityX.builder().withName("x5").build());
+        EntityX x3 = entityXDao.create(EntityX.builder().withName("x3").build());
 
-        assertThat(thrown.getValidationResults(), containsInAnyOrder(allOf(
-                hasProperty("code", equalTo("MISSING_REQUIRED_RELATION")),
-                hasProperty("location", equalTo("singleRequiredConA")))
-        ));
+        EntityY y9 = entityYDao.create(EntityY.builder().withName("y9").build());
+        EntityY y7 = entityYDao.create(EntityY.builder().withName("y7").build());
+        EntityY y4 = entityYDao.create(EntityY.builder().withName("y4").build());
+        EntityY y3 = entityYDao.create(EntityY.builder().withName("y3").build());
+        EntityY y2 = entityYDao.create(EntityY.builder().withName("y2").build());
+        EntityY y1 = entityYDao.create(EntityY.builder().withName("y1").build());
+
+        EntityY y8 = entityYDao.create(EntityY.builder().withName("y8").withYx(x13).build());
+        EntityX x12 = entityXDao.create(EntityX.builder().withName("x12").withY(y9).build());
+        EntityX x4 = entityXDao.create(EntityX.builder().withName("x4").withY(y4).build());
+
+        EntityX x10 = entityXDao.create(EntityX.builder().withName("x10").withYs(List.of(y7, y8)).build());
+        EntityX x2 = entityXDao.create(EntityX.builder().withName("x2").withX(x5).withXs(List.of(x3, x4)).build());
+
+        EntityY y6 = entityYDao.create(EntityY.builder().withName("y6").withYxs(List.of(x11, x12)).build());
+        EntityY y5 = entityYDao.create(EntityY.builder().withName("y5").withYx(x10).build());
+
+        EntityX x9 = entityXDao.create(EntityX.builder().withName("x9").withYs(List.of(y5, y6)).build());
+        EntityX x7 = entityXDao.create(EntityX.builder().withName("x7").withXs(List.of(x8, x9)).build());
+        EntityX x6 = entityXDao.create(EntityX.builder().withName("x6").withX(x7).build());
+
+        EntityX x1 = entityXDao.create(EntityX.builder().withName("x1").withX(x2).withXs(List.of(x6)).withY(y1).withYs(List.of(y2, y3)).build());
+
+        assertEquals("x1", x1.getName().orElseThrow());
+        assertEquals("x2", x1.getX().orElseThrow().getName().orElseThrow());
+        assertEquals("x6", x1.getXs().get(0).getName().orElseThrow());
+        assertEquals("y1", x1.getY().orElseThrow().getName().orElseThrow());
+        assertEquals(1, x1.getYs().stream().filter(y -> "y2".equals(y.getName().orElseThrow())).count());
+        assertEquals(1, x1.getYs().stream().filter(y -> "y3".equals(y.getName().orElseThrow())).count());
+        assertTrue(x1.getYs().stream().anyMatch(y -> "y2".equals(y.getName().orElseThrow())));
+        assertTrue(x1.getYs().stream().anyMatch(y -> "y3".equals(y.getName().orElseThrow())));
+
+        EntityX x2Test = x1.getX().orElseThrow();
+
+        assertEquals("x5", x2Test.getX().orElseThrow().getName().orElseThrow());
+        assertEquals(2, x2Test.getXs().stream().count());
+        assertTrue(x2Test.getXs().stream().anyMatch(y -> "x3".equals(y.getName().orElseThrow())));
+        assertTrue(x2Test.getXs().stream().anyMatch(y -> "x4".equals(y.getName().orElseThrow())));
+        assertFalse(x2Test.getY().isPresent());
+        assertEquals(0, x2Test.getYs().size());
+
+        EntityX x6Test = x1.getXs().get(0);
+        assertEquals("x7", x6Test.getX().orElseThrow().getName().orElseThrow());
+        assertEquals(0, x6Test.getXs().size());
+        assertFalse(x6Test.getY().isPresent());
+        assertEquals(0, x6Test.getYs().size());
+
+        EntityX x4Test = x2Test.getXs().stream().filter(c -> "x4".equals(c.getName().orElseThrow())).findFirst().orElseThrow();
+
+        assertEquals("y4", x4Test.getY().orElseThrow().getName().orElseThrow());
+        assertFalse(x4Test.getX().isPresent());
+        assertEquals(0, x4Test.getXs().size());
+        assertEquals(0, x4Test.getYs().size());
+
+        EntityX x7Test = x6Test.getX().orElseThrow();
+
+        assertEquals(2, x7Test.getXs().stream().count());
+        assertTrue(x7Test.getXs().stream().anyMatch(y -> "x8".equals(y.getName().orElseThrow())));
+        assertTrue(x7Test.getXs().stream().anyMatch(y -> "x9".equals(y.getName().orElseThrow())));
+        // It should be Optional.empty, but it is null
+        //assertFalse(x7Test.getX().isPresent());
+        assertFalse(x7Test.getY().isPresent());
+        assertEquals(0, x7Test.getYs().size());
+
+        EntityX x9Test = x7.getXs().stream().filter(c -> "x9".equals(c.getName().orElseThrow())).findFirst().orElseThrow();
+
+        assertEquals(2, x9Test.getYs().stream().count());
+        assertTrue(x9Test.getYs().stream().anyMatch(y -> "y5".equals(y.getName().orElseThrow())));
+        assertTrue(x9Test.getYs().stream().anyMatch(y -> "y6".equals(y.getName().orElseThrow())));
+        assertFalse(x9Test.getX().isPresent());
+        assertFalse(x9Test.getY().isPresent());
+        assertEquals(0, x9Test.getXs().size());
+
+        EntityY y5Test = x9Test.getYs().stream().filter(c -> "y5".equals(c.getName().orElseThrow())).findFirst().orElseThrow();
+
+        assertEquals("x10", y5Test.getYx().orElseThrow().getName().orElseThrow());
+        assertEquals(0, y5Test.getYxs().size());
+
+        EntityY y6Test = x9Test.getYs().stream().filter(c -> "y6".equals(c.getName().orElseThrow())).findFirst().orElseThrow();
+
+        assertEquals(2, y6Test.getYxs().stream().count());
+        assertTrue(y6Test.getYxs().stream().anyMatch(y -> "x11".equals(y.getName().orElseThrow())));
+        assertTrue(y6Test.getYxs().stream().anyMatch(y -> "x12".equals(y.getName().orElseThrow())));
+        assertFalse(y6Test.getYx().isPresent());
+
+        EntityX x10Test = y5Test.getYx().orElseThrow();
+
+        assertEquals(2, x10Test.getYs().stream().count());
+        assertTrue(x10Test.getYs().stream().anyMatch(y -> "y7".equals(y.getName().orElseThrow())));
+        assertTrue(x10Test.getYs().stream().anyMatch(y -> "y8".equals(y.getName().orElseThrow())));
+        assertFalse(x10Test.getX().isPresent());
+        assertFalse(x10Test.getY().isPresent());
+        assertEquals(0, x10Test.getXs().size());
+
+        EntityX x12test = y6.getYxs().stream().filter(c -> "x12".equals(c.getName().orElseThrow())).findFirst().orElseThrow();
+
+        assertEquals("y9", x12test.getY().orElseThrow().getName().orElseThrow());
+        assertFalse(x12test.getX().isPresent());
+        assertEquals(0, x12test.getXs().size());
+        assertEquals(0, x12test.getYs().size());
     }
 }
