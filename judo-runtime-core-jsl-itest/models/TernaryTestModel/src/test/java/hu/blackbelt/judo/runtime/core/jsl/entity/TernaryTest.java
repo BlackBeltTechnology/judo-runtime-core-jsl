b2b52ac1806577acd7deea16d6bee9150ac483a3
gaborflorian <gabor.florian@blackbelt.hu>
2024-02-20 21:03:16 +0100

    implement the SwitchTest from runtime-core-esm-itest

--- judo-runtime-core-jsl-itest/models/TernaryTestModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/TernaryTest.java
+++ judo-runtime-core-jsl-itest/models/TernaryTestModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/TernaryTest.java
@@ -24,6 +24,7 @@ import com.google.inject.Inject;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.ternarytest.ternarytest.aaa.AAA;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.ternarytest.ternarytest.aaa.AAADao;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.ternarytest.ternarytest.aaa.AAAForCreate;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.ternarytest.ternarytest.enum_.Enum;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.guice.TernaryTestDaoModules;
 import hu.blackbelt.judo.requirement.report.annotation.Requirement;
 import hu.blackbelt.judo.runtime.core.jsl.fixture.JudoRuntimeExtension;
@@ -31,7 +32,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 public class TernaryTest {
@@ -45,7 +53,12 @@ public class TernaryTest {
     @Test
     @Requirement(reqs = {
             "REQ-TYPE-001",
+            "REQ-TYPE-002",
             "REQ-TYPE-004",
+            "REQ-TYPE-005",
+            "REQ-TYPE-006",
+            "REQ-TYPE-007",
+            "REQ-TYPE-009",
             "REQ-ENT-001",
             "REQ-ENT-008",
             "REQ-EXPR-001",
@@ -54,7 +67,33 @@ public class TernaryTest {
             "REQ-SYNT-005"
     })
     public void testTernaries() {
-        AAA a = aDao.create(AAAForCreate.builder().build());
+
+        final String stringValue = "STRING";
+        final Integer integerValue = 10;
+        final Double doubleValue = 3.14159265;
+        final LocalDate dateValue = LocalDate.of(2020, 10, 20);
+        final LocalDateTime timestampValue = LocalDateTime.of(2020, 10, 20, 16, 30, 5, 0);
+
+        AAA a = aDao.create(AAAForCreate.builder()
+                .withStringR(stringValue)
+                .withIntegerR(integerValue)
+                .withDoubleR(BigDecimal.valueOf(doubleValue))
+                .withBooleanR(true)
+                .withDateR(dateValue)
+                .withTimestampR(timestampValue)
+                .withEnumR(Enum.Literal1)
+                .build()
+        );
+
+        assertThat(a.getConstantString().orElseThrow(), equalTo("X"));
+        assertThat(a.getString().orElseThrow(), equalTo(stringValue));
+        assertThat(a.getInteger().orElseThrow(), equalTo(integerValue));
+        assertThat(a.getDouble().orElseThrow(), equalTo(BigDecimal.valueOf(doubleValue)));
+        assertThat(a.getBoolean().orElseThrow(), equalTo(true));
+        assertThat(a.getDate().orElseThrow(), equalTo(dateValue));
+        assertThat(a.getTimestamp().orElseThrow(), equalTo(timestampValue));
+        assertThat(a.getUnknownCondition().orElseThrow(), equalTo(stringValue));
+
 
         assertEquals("true", a.getTs().orElseThrow());
         assertEquals("aaa", a.getTs1().orElseThrow());
@@ -70,6 +109,35 @@ public class TernaryTest {
 //        assertTrue(t2.isEmpty());
 //        List<BBB> t3 = aDao.getT3(a);
 //        assertTrue(t3.isEmpty());
+
+        AAA a1 = aDao.create(AAAForCreate.builder()
+                .withStringR(stringValue)
+                .withIntegerR(integerValue)
+                .withDoubleR(BigDecimal.valueOf(doubleValue))
+                .withBooleanR(false)
+                .withDateR(dateValue)
+                .withTimestampR(timestampValue)
+                .withEnumR(Enum.Literal2)
+                .build()
+        );
+
+        assertThat(a1.getConstantString().orElseThrow(), equalTo("Y"));
+        assertThat(a1.getConstantString1().orElseThrow(), equalTo("Y"));
+        assertTrue(a1.getString().isEmpty());
+        assertTrue(a1.getInteger().isEmpty());
+        assertTrue(a1.getDouble().isEmpty());
+        assertTrue(a1.getBoolean().isEmpty());
+        assertTrue(a1.getBooleanO().isEmpty());
+        assertTrue(a1.getDate().isEmpty());
+        assertTrue(a1.getTimestamp().isEmpty());
+        assertThat(a1.getUnknownCondition().orElseThrow(), equalTo(stringValue));
+
+
+        // TODO https://blackbelt.atlassian.net/browse/JNG-5543
+        //assertTrue(a1.getFirstLetterIsUpper().isEmpty());
+        //assertTrue(a1.getFirstLetterIsUpperWithDefault().isPresent());
+
+
     }
 
 }
