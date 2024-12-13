694bba8635ec3c27f4a416212e0ed52231a41377
Bence Gelei <54545024+bencegelei@users.noreply.github.com>
2024-12-13 21:26:19 +0100

    JNG-6027 Fix navigation expression (#336)
    [2024-12-13 17:21:35 +0100] bence.gelei: missing assertion
    [2024-12-13 17:11:26 +0100] bence.gelei: test case added for bug report
    [2024-12-13 16:26:58 +0100] bence.gelei: JNG-6027 [Release] Updating versions
    [2024-12-13 15:55:39 +0100] bence.gelei: Merge branch 'develop' into feature/JNG-6027_Fix_navigation_expression
    [2024-12-13 14:57:01 +0100] bence.gelei: test navigation in filter

--- judo-runtime-core-jsl-itest/models/FilterCountModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/FilterCountTest.java
+++ judo-runtime-core-jsl-itest/models/FilterCountModel/src/test/java/hu/blackbelt/judo/runtime/core/jsl/entity/FilterCountTest.java
@@ -20,6 +20,9 @@ package hu.blackbelt.judo.runtime.core.jsl.entity;
  * #L%
  */
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filtercountmodel.a.A;
@@ -32,6 +35,7 @@ import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filter
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filtercountmodel.c.C;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filtercountmodel.c.CDao;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filtercountmodel.c.CForCreate;
+import hu.blackbelt.judo.psm.generator.sdk.core.test.api.filtercountmodel.filtercountmodel.d.DForCreate;
 import hu.blackbelt.judo.psm.generator.sdk.core.test.guice.FilterCountModelDaoModules;
 import hu.blackbelt.judo.runtime.core.jsl.fixture.JudoRuntimeExtension;
 import hu.blackbelt.judo.runtime.core.jsl.fixture.JudoRuntimeFixture;
@@ -39,9 +43,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 public class FilterCountTest {
@@ -62,23 +64,27 @@ public class FilterCountTest {
     void testAncestorFilterCount(JudoRuntimeFixture fixture) {
         // Filter on the entity attributes inherited from the ancestor and count the matched ones.
 
-        C c = cDao.create(CForCreate.builder().withNum(2).build());
+        C c = createC(2, LocalDate.of(2024, 12, 13));
+        C c1 = createC(10, LocalDate.of(2024, 12, 14));
+        C c2 = createC(15);
 
         B b = bDao.create(BForCreate.builder().withName("B").build());
 
         A a = aDao.create(AForCreate.builder()
-                .withName("A")
-                .withAncestorName("PA")
-                .withNumb(2)
-                .withC(c)
-                .withCs(List.of(c))
-                .withAncestorB(b).build());
+                                    .withName("A")
+                                    .withAncestorName("PA")
+                                    .withNumb(2)
+                                    .withC(c)
+                                    .withCs(List.of(c, c1, c2))
+                                    .withAncestorB(b)
+                                    .build());
+        assertEquals(LocalDate.of(2024, 12, 13), aDao.queryEarliestC(a).orElseThrow().getD().orElseThrow().getDate().orElseThrow());
+        assertEquals(LocalDate.of(2024, 12, 14), aDao.queryLatestC(a).orElseThrow().getD().orElseThrow().getDate().orElseThrow());
 
         assertEquals("A", a.getName().orElseThrow());
         assertEquals("PA", a.getAncestorName().orElseThrow());
         assertEquals("B", a.getAncestorBName().orElseThrow());
         assertEquals("B", a.getBname().orElseThrow());
-
 
         log.debug("\n" + Strings.repeat("=", 40) + "\nFilter by: this.ancestorBName!like('B')");
         assertEquals(1, aDao.query().filterBy("this.ancestorBName!like('B')").maskedBy(AMask.aMask()).selectList().size());
@@ -96,9 +102,21 @@ public class FilterCountTest {
         assertEquals(1, aDao.query().filterBy("this.cnum == 2").maskedBy(AMask.aMask()).selectList().size());
         assertEquals(1, aDao.query().filterBy("this.cnum == 2").count());
         log.debug("\n" + Strings.repeat("=", 40) + "\nFilter by: this.sum == 2");
-        assertEquals(1, aDao.query().filterBy("this.sum == 2").maskedBy(AMask.aMask()).selectList().size());
-        assertEquals(1, aDao.query().filterBy("this.sum == 2").count());
+        assertEquals(1, aDao.query().filterBy("this.sum == 27").maskedBy(AMask.aMask()).selectList().size());
+        assertEquals(1, aDao.query().filterBy("this.sum == 27").count());
+    }
 
+    private C createC(int num, LocalDate date) {
+        return cDao.create(CForCreate.builder()
+                                     .withNum(num)
+                                     .withD(DForCreate.builder().withDate(date).build())
+                                     .build());
+    }
+
+    private C createC(int num) {
+        return cDao.create(CForCreate.builder()
+                                     .withNum(num)
+                                     .build());
     }
 
 }
